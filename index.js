3b081c7ca91a39627615cceb827bdcc5a4ccc734
hicham-mirali <56586172+hicham-mirali@users.noreply.github.com>
2019-10-20 21:20:34 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,60 @@
+const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+var panneObject = require ('./codesPanneUGC.json');
+let serviceAccount = require('./pannedata-firebase-adminsdk-ibtnm-0641528236.json');
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+});
+
+let db = admin.firestore();
+
+let docRef = db.collection('Z2M-103');
+
+
+exports.helloooPubSub = functions.pubsub.topic('gateway_telemetry').onPublish((message) => {
+//exports.helloPubSub = (pubSubEvent, context)=> {  
+  let  Code , Voiture , equi, date, Details = null, state, MyCode;
+
+  
+  try {
+    
+    Code = message.json.Code;
+    Voiture = message.json.Voiture;
+    equi = message.json.equi;
+    date = message.json.date;
+    state = message.json.State;
+
+
+
+    for (var i in panneObject){
+        var code = panneObject[i].code;
+        var details = panneObject[i].details;
+        if (code === Code){
+              Details = details;
+		} else {
+			
+		}
+	}
+   
+ 
+         let setAda = docRef.add({
+         Code: Code,
+         Voiture : Voiture,
+         Equipement : equi ,
+         Date : date,
+         Details : Details,
+         State : state, 
+    
+         });
+ }
+
+  catch (e) {
+
+    console.error('une erreur', e);
+
+  }
+  
+ 
+  
+});
